f6a26fb557adc7380f8389b10fcc9f6f76303663
dev-rome <haynesjerome@gmail.com>
2023-05-13 10:50:28 -0400

    added navbar component to handle dark and light mode switch

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,9 @@
-import { useState, useEffect } from "react";
 import { BsMoon, BsMoonFill } from "react-icons/bs";
+import useThemeContext from "../useThemeContext";
 import ToggleTheme from "./ToggleTheme";
 
 function Navbar() {
-  const [theme, setTheme] = useState("light");
-
-  useEffect(() => {
-    if (
-      localStorage.theme === "dark" ||
-      (!("theme" in localStorage) &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches)
-    ) {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
-  }, []);
-
-  useEffect(() => {
-    if (localStorage.getItem("theme") === "dark") {
-      document.documentElement.classList.add("dark");
-      setTheme("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-      setTheme("light");
-    }
-  }, [theme]);
-
-  const handleThemeSwitch = () => {
-    if (localStorage.getItem("theme") === "light") {
-      setTheme("dark");
-      localStorage.setItem("theme", "dark");
-    } else {
-      setTheme("light");
-      localStorage.setItem("theme", "light");
-    }
-  };
+  const { theme, handleThemeSwitch } = useThemeContext();
 
   return (
     <nav className="flex justify-between items-center py-8 px-4 bg-white dark:bg-[#2B3844] md:py-7 md:px-20 drop-shadow-[0_4px_2px_rgba(0,0,0,0.562)]">
