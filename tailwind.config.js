10fe99390c946e433e08317bb0e179c3c96bb369
dev-rome <haynesjerome@gmail.com>
2023-05-13 10:45:10 -0400

    setup tailwind file for dark mode and added new font

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      Nunito: ["Nunito Sans", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
