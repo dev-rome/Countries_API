0ae19733f95ae960f0829e5c0fec692e1fc6edf6
dev-rome <haynesjerome@gmail.com>
2023-05-13 10:46:11 -0400

    created useThemeContext to handle dark and light mode

---
+++ src/useThemeContext.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+
+function useThemeContext() {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, []);
+
+  useEffect(() => {
+    if (localStorage.getItem("theme") === "dark") {
+      document.documentElement.classList.add("dark");
+      setTheme("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      setTheme("light");
+    }
+  }, [theme]);
+
+  const handleThemeSwitch = () => {
+    if (localStorage.getItem("theme") === "light") {
+      setTheme("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      setTheme("light");
+      localStorage.setItem("theme", "light");
+    }
+  };
+
+  return { theme, handleThemeSwitch };
+}
+
+export default useThemeContext;
