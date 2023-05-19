551cd31a3bfed90ff7446ec80970ba95283a4f0a
dev-rome <haynesjerome@gmail.com>
2023-05-18 22:41:17 -0400

    added countries component to display all countries

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,8 +1,23 @@
-function Countries() {
+import Card from "./Card";
+
+function Countries({ country }) {
+  const { flags, name, population, region, capital } = country;
+  const numberFormat = population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
   return (
-    <section className="h-screen bg-white dark:bg-[#202C36]">
-      <p>Hello</p>
-    </section>
+    <Card className="w-full max-w-[265px] mx-auto dark:bg-[#2B3844] bg-[#FFFFFF] rounded">
+      <img className="w-full h-[160px] object-cover" src={flags.png} alt={flags.alt} />
+      <div className="px-6 pb-11">
+        <h1 className="font-Nunito text-lg font-extrabold dark:text-[#FFFFFF] text-[#111517] mt-6">
+          {name.common}
+        </h1>
+        <ul className="font-Nunito dark:text-[#FFFFFF] text-[#111517] mt-4">
+          <li>Population: {numberFormat}</li>
+          <li className="mt-2">Region: {region}</li>
+          <li className="mt-2">Capital: {capital}</li>
+        </ul>
+      </div>
+    </Card>
   );
 }
 
