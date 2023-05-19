c552eb0e97b8f2779c502f8560ee9b973ff0620b
dev-rome <haynesjerome@gmail.com>
2023-05-19 12:36:29 -0400

    added country info page

---
+++ src/pages/CountryInfoPage.js
@@ -0,0 +1,24 @@
+import { useParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
+import Country from "../components/Country";
+
+function CountrieInfoPage() {
+  const { name } = useParams();
+  const url = `https://restcountries.com/v3.1/name/${name}`;
+  const { data, loading, error } = useFetch(url);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
+  return (
+    <section className="min-h-screen bg-[#F2F2F2] dark:bg-[#202C36]">
+      <div className="max-w-7xl mx-auto">
+          {data.map((country, index) => (
+            <Country key={index} country={country} />
+          ))}
+        </div>
+    </section>
+  );
+}
+
+export default CountrieInfoPage;
