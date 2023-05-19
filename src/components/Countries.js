f3eb2b8369fcfcbd1776c903c60537e8f6b4cc2d
dev-rome <haynesjerome@gmail.com>
2023-05-19 12:38:08 -0400

    added link, format population number, and check for capital

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,23 +1,33 @@
+import { Link } from "react-router-dom";
 import Card from "./Card";
 
 function Countries({ country }) {
   const { flags, name, population, region, capital } = country;
-  const numberFormat = population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  const numberFormat = population
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  const capitalCheck = capital ? capital : "N/A";
 
   return (
-    <Card className="w-full max-w-[265px] mx-auto dark:bg-[#2B3844] bg-[#FFFFFF] rounded">
-      <img className="w-full h-[160px] object-cover" src={flags.png} alt={flags.alt} />
-      <div className="px-6 pb-11">
-        <h1 className="font-Nunito text-lg font-extrabold dark:text-[#FFFFFF] text-[#111517] mt-6">
-          {name.common}
-        </h1>
-        <ul className="font-Nunito dark:text-[#FFFFFF] text-[#111517] mt-4">
-          <li>Population: {numberFormat}</li>
-          <li className="mt-2">Region: {region}</li>
-          <li className="mt-2">Capital: {capital}</li>
-        </ul>
-      </div>
-    </Card>
+    <Link to={`/country/${name.common}`}>
+      <Card className="w-full max-w-[265px] mx-auto dark:bg-[#2B3844] bg-[#FFFFFF] cursor-pointer">
+        <img
+          className="w-full h-[160px] object-cover"
+          src={flags.png}
+          alt={flags.alt}
+        />
+        <div className="px-6 pb-11">
+          <h1 className="font-Nunito text-lg font-extrabold dark:text-[#FFFFFF] text-[#111517] mt-6">
+            {name.common}
+          </h1>
+          <ul className="font-Nunito dark:text-[#FFFFFF] text-[#111517] mt-4">
+            <li>Population: {numberFormat}</li>
+            <li className="mt-2">Region: {region}</li>
+            <li className="mt-2">Capital: {capitalCheck}</li>
+          </ul>
+        </div>
+      </Card>
+    </Link>
   );
 }
 
