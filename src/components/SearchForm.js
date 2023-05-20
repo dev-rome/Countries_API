6cfb0261a51ce7d9e36d7c9a1d51753b8d5583bc
dev-rome <haynesjerome@gmail.com>
2023-05-20 10:35:32 -0400

    edit search form to handle search for states

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,21 @@
-function SearchForm() {
+import { useState } from "react";
+
+function SearchForm({ onSearch }) {
+  const [search, setSearch] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    onSearch(search);
+  };
+
   return (
-    <form className="mt-6 lg:mt-12">
+    <form onSubmit={handleSubmit} className="mt-6 lg:mt-12">
       <input
         className="w-full md:w-[480px] input-bg dark:bg-[#2B3844] bg-[#FFFFFF] py-4 pl-16 rounded outline-none dark:text-[#FFFFFF] text-[#111517] dark:placeholder:text-[#FFFFFF] placeholder:text-[##C4C4C4]"
         type="text"
         placeholder="Search for a country..."
+        value={search}
+        onChange={e => setSearch(e.target.value)}
       />
     </form>
   );
