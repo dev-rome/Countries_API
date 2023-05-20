718b97f6c87dec292d22b2de22e48dc157357ee3
dev-rome <haynesjerome@gmail.com>
2023-05-20 09:40:37 -0400

    edit url for state full name, change function name

--- src/pages/CountryInfoPage.js
+++ src/pages/CountryInfoPage.js
@@ -2,9 +2,9 @@ import { useParams } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
 import Country from "../components/Country";
 
-function CountrieInfoPage() {
+function CountryInfoPage() {
   const { name } = useParams();
-  const url = `https://restcountries.com/v3.1/name/${name}`;
+  const url = `https://restcountries.com/v3.1/name/${name}?fullText=true`
   const { data, loading, error } = useFetch(url);
 
   if (loading) return <div>Loading...</div>;
@@ -13,12 +13,12 @@ function CountrieInfoPage() {
   return (
     <section className="min-h-screen bg-[#F2F2F2] dark:bg-[#202C36]">
       <div className="max-w-7xl mx-auto">
-          {data.map((country, index) => (
-            <Country key={index} country={country} />
-          ))}
-        </div>
+        {data.map((country, index) => (
+          <Country key={index} country={country} />
+        ))}
+      </div>
     </section>
   );
 }
 
-export default CountrieInfoPage;
+export default CountryInfoPage;
